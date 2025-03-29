4a1c34beec2a3a02c239e9edbc3eaffecdcd4a74
Panos Lampropoulos <5313566+panosl1@users.noreply.github.com>
2025-03-29 20:08:50 +0100

    Day 13

--- .config/nvim/lua/panosl1/plugins/snacks.lua
+++ .config/nvim/lua/panosl1/plugins/snacks.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/snacks.nvim",
     priority = 1000,
-    lazy = false,
+    lazy = true,
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
@@ -58,14 +58,14 @@ return {
         desc = "Buffers",
       },
     },
-    init = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          -- Create some toggle mappings
-          Snacks.toggle.dim():map("<leader>uD")
-        end,
-      })
-    end,
+    --    init = function()
+    --      vim.api.nvim_create_autocmd("User", {
+    --        pattern = "VeryLazy",
+    --        callback = function()
+    --          -- Create some toggle mappings
+    --          Snacks.toggle.dim():map("<leader>uD")
+    --animate        end,
+    --      })
+    --    end,
   },
 }
